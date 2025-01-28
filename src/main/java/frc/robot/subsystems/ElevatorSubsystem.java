f3e698b58d3900161ca6a78859d023bcc032d718
589CVHSfrc <CVHSfrc589@gmail.com>
2025-01-27 20:28:43 -0800

    elevator PID very close

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -57,13 +57,13 @@ public class ElevatorSubsystem extends SubsystemBase {
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
         // Set PID values for position control. We don't need to pass a closed loop
         // slot, as it will default to slot 0.
-        .p(0.1)
-        .i(0)
+        .p(0.1)//0.1
+        .i(0.00005)//0.00005
         .d(0)
         .outputRange(-.2, .2)
         // Set PID values for velocity control in slot 1
         .p(0.0001, ClosedLoopSlot.kSlot1)
-        .i(0, ClosedLoopSlot.kSlot1)
+        .i(0.0000001, ClosedLoopSlot.kSlot1)
         .d(0, ClosedLoopSlot.kSlot1)
         .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
         .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
